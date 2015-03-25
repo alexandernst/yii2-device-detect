854ab83c992d4692058dc601c427e842789d3f2f
Alexander Nestorov <alexandernst@gmail.com>
2015-03-26 00:12:29 +0100

    Initial commit

---
+++ DeviceDetect.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace alexandernst\devicedetect;
+
+use Yii;
+use Detection\MobileDetect;
+
+/**
+ * DeviceDetect
+ *
+ * @author Alexander Nestorov <alexandernst@gmail.com>
+ * @version 0.0.1
+ */
+
+class Component extends \yii\base\Component {
+
+	private $_mobileDetect;
+
+	public function __call($name, $parameters) {
+		return call_user_func_array(
+			array($this->_mobileDetect, $name),
+			$parameters
+		);
+	}
+
+	public function __construct($config = array()) {
+		parent::__construct($config);
+	}
+
+	public function init() {
+		$this->_mobileDetect = new Mobile_Detect();
+		parent::init();
+	}
+
+}
